8fde07245035d2703c21a9d5f3cff0f887ad9cbf
Andrew Kanner <andrew.kanner@gmail.com>
2013-11-02 23:35:06 +0400

    Chapter1: solutions 1-6 and 1-7 added

---
+++ Chapter1/1-6/eof_value.c
@@ -0,0 +1,19 @@
+/*
+ * Exercise 1-6, page 19
+ * Упражнение 1.6, страница 31
+ * 
+ * Verify that the expression getchar() != EOF is 0 or 1.
+ *
+ * Проверьте, что выражение getchar() != EOF действительно равно 1 или 0.
+ */
+
+#include <stdio.h>
+
+int main (void) {
+	printf("Enter some character.\n");
+
+	// You can try to enter Ctrl+D or Ctrl+Z (EOF)
+
+	printf("'getchar() != EOF' value : %d\n", getchar() != EOF);
+	return 0;
+}
---
+++ Chapter1/1-7/eof_value.c
@@ -0,0 +1,15 @@
+/*
+ * Exercise 1-7, page 19
+ * Упражнение 1.7, страница 31
+ * 
+ * Write a program to print the value of EOF.
+ *
+ * Напишите программу для вывода значения константы EOF.
+ */
+
+#include <stdio.h>
+
+int main (void) {
+	printf("EOF value : %d\n", EOF);
+	return 0;
+}
