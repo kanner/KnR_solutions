ca0366c1fa9ada557c3b7febc273c98a0c69d9ea
Andrew Kanner <andrew.kanner@gmail.com>
2013-11-06 21:15:53 +0400

    Chapter1: solution 1-13 contained an error, changes inserted
    1-13 contained vertical and horizontal histograms for numbers (1-9), spaces and tabs.
    But the task was to perform histograms for LENGTH of the WORDS in the input.

--- Chapter1/1-13/length_histogram.c
+++ Chapter1/1-13/length_histogram.c
@@ -11,81 +11,70 @@
 
 #include <stdio.h>
 
+#define	IN		1
+#define OUT		0
+#define MAXWORDLEN	10
+
 /* подсчет цифр, символов пустого пространства, остальных */
 int main (void)
 {
-	int height = 0;	// the height of vertical histogram
-	int c, i, nwhite, nother;
-	int ndigit[10];
-	
-	nwhite = nother = 0;
-	for (i = 0; i < 10; i++)
-		ndigit[i] = 0;
+	int c, i;
+	int word_length = 0;
+	int height = 0;		// the height of vertical histogram
+	int state = OUT;
+	// array of word length`s, [MAXWORDLEN] - length above MAXWORDLEN
+	int length[MAXWORDLEN+1]={0};
+	c = i = 0;
 
-	while ((c = getchar()) != EOF)
-		if (c >= '0' && c <= '9') {
-			ndigit[c - '0']++;
-			if (ndigit[c - '0'] > height)		// needed for vertical histogram
-				height = ndigit[c - '0'];
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+			if (word_length <= 10)
+				length[word_length-1]++;
+			else
+				length[MAXWORDLEN]++;
+			word_length = 0;
 		}
-		else if (c == ' ' || c == '\n' || c == '\t') {
-			nwhite++;
-			if (nwhite > height)			// needed for vertical histogram
-				height = nwhite;
+		else if (state == OUT) {
+			state = IN;
+			word_length++;
 		}
-		else {
-			nother++;
-			if (nother > height)
-				height = nother;
+		else if (state == IN) {
+			word_length++;
 		}
+	}
 
-	printf("digits =");
-	for (i = 0; i < 10; i++)
-		printf(" %d", ndigit[i]);
-	printf(", white space = %d, other = %d\n",
-		nwhite, nother);
-
-	printf("max = %d\n", height);
+	printf("length =");
+	for (i = 0; i <= 10; i++) {
+		if (length[i]>height)	// needed for vertical histogram
+			height = length[i];
+		printf(" %d", length[i]);
+	}
+	printf("\nmax = %d\n", height);
 
 	printf("\nhorizontal histogram:\n");
 	int j;
-	for (i = 0; i < 10; i++) {
-		printf("digit %d:\t", i+1);
-		for (j = 1; j <= ndigit[i]; j++)
+	for (i = 0; i <= 10; i++) {
+		if (i < 10)
+			printf("len=%d\t[%d]:\t", i+1, length[i]);
+		else
+			printf("len>10\t[%d]:\t", length[i]);
+		for (j = 1; j <= length[i]; j++)
 			printf("+");
 		printf("\n");
 	}
 	
-	printf("white:\t\t");
-	for (j = 1; j <= nwhite; j++)
-		printf("+");
-	printf("\n");
-
-	printf("other:\t\t");
-	for (j = 1; j <= nother; j++)
-		printf("+");
-        printf("\n");
-
 	printf("\nvertical histogram:\n");
-	int k;
-	for (k = 0; k <= height; k++) {
-		for (i = 0; i < 10; i++ ) {			// print digits 0-9 at first
-			if (k == 0)
-				printf("%d\t", i+1);
-			else if (ndigit[i] >= k)
+	for (i = 0; i <= height; i++) {
+		for (j = 0; j <= 10; j++ ) {
+			if (i == 0) {	// header
+				if (j == 10)
+					printf(">10 [%d]\t", length[j]);
+				else
+					printf("%d [%d]\t", j+1, length[j]);
+			}
+			else if (length[j] >= i)
 				printf ("+\t");
-			else
-				printf("\t");
-		}
-		if (k == 0)					// header extension
-			printf("white\tother");
-		else {
-			if (nwhite >= k)			// then print white spaces
-				printf("+\t");
-			else
-				printf("\t");
-			if (nother >= k)			// then print other
-				printf("+\t");
 			else
 				printf("\t");
 		}
