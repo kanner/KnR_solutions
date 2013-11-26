de38e471053ed68c657cf4c30990a1101ffd129c
Andrew Kanner <andrew.kanner@gmail.com>
2013-11-26 10:40:58 +0400

    Chapter2: solution 2-2 added

---
+++ Chapter2/2-2/loop_alt.c
@@ -0,0 +1,39 @@
+/**
+ * Exercise 2-2, page 40
+ * Упражнение 2.2, страница 56
+ * 
+ * Write a loop equivalent to the for loop above without using && or ||.
+ *
+ * Напишите цикл, эквивалентный приведенному выше циклу for, не используя операции && и ||.
+ * 
+ */
+
+#include <stdio.h>
+
+#define LENGTH	20
+
+int main (void) {
+	int i = 0;
+	int lim, c;
+	lim = LENGTH;
+	char s[LENGTH] = {0};
+
+	/** real loop */
+//	for (i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+//		s[i] = c;
+
+	/** loop equivalent */
+	while (i<lim-1) {
+		if ((c = getchar()) == '\n')
+			break;
+		else if (c == EOF)
+			break;
+		else
+			s[i++] = c;
+	}
+
+	s[i] = '\0';
+    printf("%s\n", s);
+	
+	return 0;
+}
