5b4bf4948306cc01be956b68a1b28187f6e633ad
Andrew Kanner <andrew.kanner@gmail.com>
2015-09-21 14:28:56 +0300

    Chapter5: final code refactoring in solution 5-13

--- Chapter5/5-13/tail.c
+++ Chapter5/5-13/tail.c
@@ -1,7 +1,7 @@
 /**
  * Exercise 5-13, page 98
  * Упражнение 5.13, страница 129
- * 
+ *
  * Write the program tail, which prints the last n lines of its input. By default, n
  * is set to 10, let us say, but it can be changed by an optional argument so that
  * tail -n
@@ -22,119 +22,183 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
-
 #include <string.h>
+#include <stdlib.h>
+
+// error codes
+#define	STRING_TOO_LONG	1
+#define	CACHE_TOO_SMALL	2
 
 #define TAIL 10
 #define MAXLEN 1000
-
-/** for alloc(), afree() */
-#define ALLOCSIZE 10000
-static char allocbuf[ALLOCSIZE];
-static char *allocp = allocbuf;
-
-char *alloc(int n) {
-	if (allocbuf + ALLOCSIZE - allocp >= n) {
-		allocp += n;
-		return allocp - n;
-	}
-	else
-		return 0;
-}
-
-void afree(char *p) {
-	if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
-		allocp = p;
-}
 
 /** Ex. 5-6 */
 int _getline_ptr(char *s, int lim) {
 	char c = '\0';
 	char *p = s;
-	
+
 	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
 		*s++ = c;
 	if (c == '\n')
 		*s++ = c;
+	/** error checking */
+	else if (c != EOF)
+		return -1;
 	*s = '\0';
 	return s - p;
 }
 
-void line_shift(char *lineptr[], int shift_size, int nlines) {
+void cleanlines(char *lineptr[], int nlines) {
 	int i;
-
-	strcpy(allocbuf, allocbuf + shift_size);
-//	allocp -= shift_size;
-
-	for (i = 1; i < nlines - 1; i++)
-		lineptr[i] = lineptr[i+1];
+	for (i = 0; i < nlines; i++)
+		lineptr[i]='\0';
 }
 
-int readlines_tail(char *lineptr[], int ntail) {
+void cleancache(char *cache, int cache_size) {
+	int i;
+	for (i = 0; i < cache_size; i++)
+		cache[i]='\0';
+}
+
+void clean_line(char *lineptr, int length) {
+	int i;
+	for (i = 0; i < length; i++)
+		lineptr[i]='\0';
+}
+
+void shift_queue(char *lineptr[], char *cache, int ntail) {
+	int len = strlen(lineptr[0]) + 1;
+	int element = 0;
+	char *p = cache;
+
+	/** empty the first element memory */
+	clean_line(lineptr[0], len);
+
+	/** shift cache to the left */
+	while (element != ntail-1) {
+		element++;
+		int _len = strlen(lineptr[element]) + 1;
+
+		/** string size could be more then previous one, so we need to copy to buffer */
+		char tmp[_len];
+
+		strcpy(tmp,lineptr[element]);
+		/** order: we should clean first, then print at previous position (there can be overlap, so we can clean new data) */
+		clean_line(lineptr[element], _len);
+		strcpy(p, tmp);
+//		clean_line(lineptr[element], _len);
+
+		/** update element pointer in lineptr */
+//		lineptr[element] = NULL;
+		lineptr[element-1] = p;
+		p = p + _len;
+//		lineptr[element] = p;
+	}
+
+	return;
+
+}
+
+int readlines_tailqueue(char *lineptr[], int *ntail, char *cache, int cache_size) {
 	int len, nlines;
 	char *p, line[MAXLEN];
+	int cache_len = strlen(cache);
+	int _ntail = *ntail;
+
+	/** first empty position */
+	p = cache + strlen(cache);
 
 	nlines = 0;
 	while ((len = _getline_ptr(line, MAXLEN)) > 0) {
-		if (nlines >= ntail) {
-			line_shift(lineptr, strlen(lineptr[0])+1, nlines);
-//			nlines--;
-			if ((p = alloc(len)) == NULL)
-				return -1;
-			line[len - 1] = '\0';
-			strcpy(p, line);
-//			lineptr[nlines++] = p;
-			lineptr[nlines - 1] = p;
+		/** tail-limit reached -- we should first shift cache and lineptr elements */
+		if (nlines >= _ntail) {
+			p -= strlen(lineptr[0])+1;
+			cache_len -= strlen(lineptr[0])+1;
+			shift_queue(lineptr, cache, _ntail);
+			nlines--;
 		}
-		else if ((p = alloc(len)) == NULL) {
-			return -1;
-		}
+
+		/** populate cache */
+		if ((cache_len + len + 1) > cache_size)
+			return CACHE_TOO_SMALL;
 		else {
-			line[len - 1] = '\0';
+			line[len] = '\0';
 			strcpy(p, line);
 			lineptr[nlines++] = p;
+			/** next empty position */
+			p += len+1;
+			cache_len += len+1;
 		}
 	}
+
+	/** error checking */
+	if (len == -1) {
+		*ntail = 0;
+		return STRING_TOO_LONG;
+	}
+
+	/** update ntail in orfer to pass to writelines() */
+	*ntail = nlines;
 	return 0;
 }
 
-void writelines(char *lineptr[], int nlines) {
+void writelines(char *lineptr[], int nlines, char *cache, int cache_size) {
 	int i;
-	printf("\nlines entered:\n");
+//	printf("\nlines entered:\n");
 	for (i = 0; i < nlines; i++)
-		printf("[%d]\t%s\n", i, lineptr[i]);
-
+		// we can write without '\n', cause '\n' is at the end of cached elements
+		printf("%s", *lineptr++);
+//		printf("%s\n", *lineptr++);
+//		printf("[%d]\t%s\n", i, *lineptr++);
+/*
 	printf("\nraw memory:\n");
-	for (i = 0; i < ALLOCSIZE; i++)
-		printf("%c", allocbuf[i]);
+	for (i = 0; i < cache_size; i++)
+		printf("%c", cache[i]);
 	printf("\n");
+*/
 }
 
 int main (int argc, char *argv[]) {
 	int res;
 	int ntail = TAIL;
 
+//	char cache[MAXCACHE] = {0};
+
 	if (argc > 2) {
 		printf("\nUsage:\n\t%s -n\n\tn -\tnumber of tailed lines\n", argv[0]);
 		return 1;
 	}
-	if (argc == 2 && argv[1][0] == '-') {
+	if (argc == 2 && *(argv+1)[0] == '-') {
 		int tmp = atoi(argv[1]+1);
 		if (tmp > 0 && tmp < INT_MAX)
 			ntail = tmp;
 	}
 
-	printf("ntail = %d\n", ntail);
+//	printf("info: ntail = %d\n", ntail);
 
+	char cache[MAXLEN*ntail];
 	char *lineptr[ntail];
-	res = readlines_tail(lineptr, ntail);
-	if (res) {
-		printf("error: input too big\n");
-		return -1;
-	}
-	writelines(lineptr, ntail);
+	cleanlines(lineptr, ntail);
+	cleancache(cache, MAXLEN*ntail);
 
-	return 0;
+	res = readlines_tailqueue(lineptr, &ntail, cache, MAXLEN*ntail);
+	if (!res) {
+		writelines(lineptr, ntail, cache, MAXLEN*ntail);
+		return 0;
+	}
+	else {
+		switch(res) {
+			case STRING_TOO_LONG:
+				printf("error: too long string in input\n");
+				break;
+			case CACHE_TOO_SMALL:
+				printf("error: cache too small to store %d strings, this shouldn`t happen...\n", ntail);
+				break;
+			default:
+				printf("error: unknown error code occured\n");
+				break;
+		}
+		return res;
+	}
 }
