bc31b955e8721a03c6bc761ed08ad5da781c11b0
Andrew Kanner <andrew.kanner@gmail.com>
2015-09-24 09:07:01 +0300

    Chapter5: code refactoring in solution 5-14
    _qsort() not changed, only func pointers changes

--- Chapter5/5-14/qsort_reverse.c
+++ Chapter5/5-14/qsort_reverse.c
@@ -15,7 +15,6 @@
 
 #define MAXLINES 5000
 #define MAXLEN 1000
-#define MAXCACHE 10000
 
 char *lineptr[MAXLINES];
 /** for alloc(), afree() */
@@ -83,12 +82,15 @@ void swap (void *v[], int i, int j) {
 }
 
 #include <stdlib.h>
+
 /* numcmp: compare s1 and s2 as numerical */
 int numcmp(char *s1, char *s2) {
 	double v1, v2;
 
 	v1 = atof(s1);
 	v2 = atof(s2);
+
+	// error checking here?
 
 	if (v1 < v2)
 		return -1;
@@ -98,27 +100,34 @@ int numcmp(char *s1, char *s2) {
 		return 0;
 }
 
+int numcmp_reverse(char *s1, char *s2) {
+	return (-1)*numcmp(s1, s2);
+}
+
+int strcmp_reverse(char *s1, char *s2) {
+	return (-1)*strcmp(s1, s2);
+}
+
 //void qsort (char *v[], int left, int right) {
-void _qsort(void *v[], int left, int right, int (*comp)(void *, void *), int reverse) {
+void _qsort(void *v[], int left, int right, int (*comp)(void *, void *)) {
 	int i, last;
 	void swap(void *v[], int i, int j);
 //	void swap(char *v[], int i, int j);
 
-	// we should keep this if-block?
 	if (left >= right)
 		return;
 	swap(v, left, (left + right) / 2);
 	last = left;
 	for (i = left + 1; i <= right; i++)
 //		if (strcmp(v[i], v[left]) < 0)
-//		if ((*comp)(v[i], v[left]) < 0)
-		if ((reverse ? ((*comp)(v[i], v[left]) > 0) : ((*comp)(v[i], v[left]) < 0)))
+//		if ((reverse ? ((*comp)(v[i], v[left]) > 0) : ((*comp)(v[i], v[left]) < 0)))
+		if ((*comp)(v[i], v[left]) < 0)
 			swap(v, ++last, i);
 	swap(v, left, last);
 //	qsort(v, left, last - 1);
-	_qsort(v, left, last - 1, comp, reverse);
+	_qsort(v, left, last - 1, comp);
 //	qsort(v, last + 1, right);
-	_qsort(v, last + 1, right, comp, reverse);
+	_qsort(v, last + 1, right, comp);
 }
 
 /** string sorting from stdin */
@@ -126,6 +135,8 @@ int main (int argc, char *argv[]) {
 	int nlines;
 	int numeric = 0;	// 1 -- string of numbers
 	int reverse = 0;	// 1 -- reverse order
+	/** compare operation */
+	int (*comp)(void*,void*) = (int (*)(void*,void*))strcmp;
 
 	if (argc > 1) {
 		int i;
@@ -145,7 +156,16 @@ int main (int argc, char *argv[]) {
 
 	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
 //		qsort(lineptr, 0, nlines - 1);
-		_qsort((void **) lineptr, 0, nlines-1, (int (*)(void*,void*))(numeric ? numcmp : strcmp), reverse);
+//		_qsort((void **) lineptr, 0, nlines-1, (int (*)(void*,void*))(numeric ? numcmp : strcmp), reverse);
+
+		if (numeric && reverse)
+			comp = (int (*)(void*,void*))numcmp_reverse;
+		else if (numeric)
+			comp = (int (*)(void*,void*))numcmp;
+		else if (reverse)
+			comp = (int (*)(void*,void*))strcmp_reverse;
+
+		_qsort((void **) lineptr, 0, nlines-1, comp);
 		printf("qsort for %s-input in %s order\n", numeric ? "numeric" : "character", reverse ? "reverse" : "straight");
 		writelines(lineptr, nlines);
 		return 0;
